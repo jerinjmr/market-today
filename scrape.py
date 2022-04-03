856d66cdc9a92373c6d7b33f5fd8a74380e45976
jerinjaimon <jerinjaimon@gmail.com>
2022-04-03 19:27:30 +0530

    feat: Add new source for news
    1. New data source
    2. Sort news as per time

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ Python web scraping technology is used to extract information from website.
 """
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 def get_stock_reco():
@@ -18,14 +19,19 @@ def get_stock_reco():
     res2 = requests.get(
         'https://www.moneycontrol.com/news/business/stocks/page-2')
 
+    res3 = requests.get(
+        'https://economictimes.indiatimes.com/markets/stocks/recos')
+
     soup1 = BeautifulSoup(res1.text, 'html.parser')
     soup2 = BeautifulSoup(res2.text, 'html.parser')
+    soup3 = BeautifulSoup(res3.text, 'html.parser')
 
     link1 = soup1.select('#cagetory')
     link2 = soup2.select('#cagetory')
     links = link1 + link2
+    link3 = soup3.select('.eachStory')
 
-    def fetch_market_news(links):
+    def fetch_market_news_01(links):
         """Fetch market feeds."""
         news = []
         for item in links:
@@ -33,11 +39,33 @@ def get_stock_reco():
             for element in level1:
                 level2 = element.a
                 title = level2.get('title')  # Gets the title
+                url = level2.get('href')
                 level3 = element.span
                 date = level3.getText()  # Gets the date and time
-                subject = (date, title)
+                date = datetime.strptime(date, '%B %d, %Y %I:%M %p %Z')
+                subject = [date, title, url]
                 news.append(subject)
         return news
 
-    market_news = fetch_market_news(links)
+    def fetch_market_news_02(links):
+        news = []
+        url = "https://economictimes.indiatimes.com/"
+        for item in links:
+            level3 = item.time
+            date = level3.getText()
+            date = datetime.strptime(date, '%b %d, %Y, %I:%M %p %Z')
+            level1 = item.h3
+            for element in level1:
+                title = element.getText()  # Gets the title
+                url = url + element.get('href')
+            subject = [date, title, url]
+            news.append(subject)
+        return news
+
+    market_news_01 = fetch_market_news_01(links)
+    market_news_02 = fetch_market_news_02(link3)
+    market_news = market_news_01 + market_news_02
+    market_news.sort(reverse=True, key=lambda x: x[0])
+    for item in market_news:
+        item[0] = item[0].strftime('%B %d, %Y %I:%M %p')
     return market_news
