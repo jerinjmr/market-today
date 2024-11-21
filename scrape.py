c89392793a2ff749b1e73b189f40c2e778a874ed
jerin <jerin@scalers.ai>
2024-11-21 14:47:28 +0530

    Fix: Remove date as workaround

--- scrape.py
+++ scrape.py
@@ -41,9 +41,10 @@ def get_stock_reco():
                 title = level2.get('title')  # Gets the title
                 url = level2.get('href')
                 level3 = element.span
-                date = level3.getText()  # Gets the date and time
-                date = datetime.strptime(date[:-4], '%B %d, %Y %I:%M %p')
-                subject = [date, title, url]
+                # date = level3.getText()  # Gets the date and time
+                # date = datetime.strptime(date[:-4], '%B %d, %Y %I:%M %p')
+                # subject = [date, title, url]
+                subject = [title, url]
                 news.append(subject)
         return news
 
@@ -65,9 +66,9 @@ def get_stock_reco():
     market_news_01 = fetch_market_news_01(links)
     market_news_02 = fetch_market_news_02(link3)
     market_news = market_news_01 + market_news_02
-    market_news.sort(reverse=True, key=lambda x: x[0])
-    for item in market_news:
-        item[0] = item[0].strftime('%B %d, %Y %I:%M %p')
+    # market_news.sort(reverse=True, key=lambda x: x[0])
+    # for item in market_news:
+    #     item[0] = item[0].strftime('%B %d, %Y %I:%M %p')
     return market_news
 
 
